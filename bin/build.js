277d76ad421684324ba0a0f58e6a1ead36b3e12f
Binoy Patel <binoypatel14@gmail.com>
2017-06-03 18:57:30 -0400

    Install Packages using npm or yarn

--- bin/build.js
+++ bin/build.js
@@ -5,6 +5,42 @@ const paths = require('path');
 const fs = require('fs');
 const figlet = require('figlet');
 const chalk = require('chalk');
+const execSync = require('child_process').execSync;
+const spawn = require('cross-spawn');
+
+function shouldUseYarn() {
+  try {
+    execSync('yarnpkg --version', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+const installPackages = () => {
+  console.log(chalk.white.bold('Installing Packages'));
+  return new Promise((resolve, reject) => {
+    let command;
+    let args = ['install'];
+
+    if (shouldUseYarn()) {
+      command = 'yarn';
+    } else {
+      command = 'npm';
+    }
+
+    const child = spawn(command, args, { stdio: 'inherit' });
+    child.on('close', code => {
+      if (code !== 0) {
+        reject({
+          command: `${command} ${args.join(' ')}`
+        });
+        return;
+      }
+      resolve();
+    })
+  })
+}
 
 const build = (appName) => {
   cp('-r', __dirname + '/../node_modules/rxapp/.', appName);
@@ -19,21 +55,27 @@ const build = (appName) => {
     console.log('----------------------------------------------------------');
     console.log(chalk.green.bold('Welcome to ReactXP'));
     console.log('----------------------------------------------------------');
-    console.log(chalk.white.bold('Let\'s get started'));
-    console.log(chalk.green('Step 1: cd into the newly created ' + appName + ' directory'));
-    console.log(chalk.green('Step 2: install dependencies using yarn or npm'));
-    console.log('----------------------------------------------------------');
-    console.log(chalk.white.bold('For Web'));
-    console.log(chalk.green('Step 1. npm run web-watch'));
-    console.log(chalk.black.bold('This compiles the TypeScript code and recompiles it whenever any files are changed.'))
-    console.log(chalk.green('Step 2. Open index.html in your browser to view the result.'));
-    console.log('----------------------------------------------------------');
-    console.log(chalk.white.bold('For React Native'));
-    console.log(chalk.green('Step 1. run npm run rn-watch'));
-    console.log(chalk.black.bold('This compiles the TypeScript code and recompiles it whenever any files are changed.'));
-    console.log(chalk.green('Step 2. run npm start'));
-    console.log(chalk.black.bold('This starts the React Native Packager.'));
-    console.log('----------------------------------------------------------');
+    cd(appName);
+    installPackages().then(() => {
+      console.log(chalk.white.bold('Let\'s get started'));
+      console.log(chalk.green('Step 1: cd into the newly created ' + appName + ' directory'));
+      console.log('----------------------------------------------------------');
+      console.log(chalk.white.bold('For Web'));
+      console.log(chalk.green('Step 1. npm run web-watch'));
+      console.log(chalk.black.bold('This compiles the TypeScript code and recompiles it whenever any files are changed.'))
+      console.log(chalk.green('Step 2. Open index.html in your browser to view the result.'));
+      console.log('----------------------------------------------------------');
+      console.log(chalk.white.bold('For React Native'));
+      console.log(chalk.green('Step 1. run npm run rn-watch'));
+      console.log(chalk.black.bold('This compiles the TypeScript code and recompiles it whenever any files are changed.'));
+      console.log(chalk.green('Step 2. run npm start'));
+      console.log(chalk.black.bold('This starts the React Native Packager.'));
+      console.log('----------------------------------------------------------');
+    })
+    .catch(error => {
+      console.log(chalk.red('An unexpected error occurred'))
+      console.log(chalk.red(error));
+    });
   });
 }
 
