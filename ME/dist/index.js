ae37985fb0460c76faa2ded0ddec847b37d66780
nader dabit <dabit3@gmail.com>
2017-06-05 22:39:23 -0500

    removed unnecessary repo

--- ME/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var RX = require("reactxp");
-var App = require("./App");
-RX.App.initialize(true, true);
-RX.UserInterface.setMainView(RX.createElement(App, null));
-//# sourceMappingURL=index.js.map
